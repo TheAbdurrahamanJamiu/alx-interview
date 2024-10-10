123271c071461ad8bd163a11ae5a80afe997b06d
TheAbdurrahamanJamiu <abdurrahamanjamiu75@outlook.com>
2024-10-10 16:05:35 -0700

    typescript

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,26 @@
+def canUnlockAll(boxes):
+    n = len(boxes)
+    unlocked = [False] * n
+    unlocked[0] = True
+    keys = boxes[0]
+    stack = keys[:]
+
+    while stack:
+        key = stack.pop()
+        if key < n and not unlocked[key]:
+            unlocked[key] = True
+            for new_key in boxes[key]:
+                if new_key not in stack and not unlocked[new_key]:
+                    stack.append(new_key)
+
+    return all(unlocked)
+
+# Example usage:
+boxes = [[1], [2], [3], []]
+print(canUnlockAll(boxes))
+
+boxes = [[1, 2], [3], [1], []]
+print(canUnlockAll(boxes))
+
+boxes = [[1], [], [0]]
+print(canUnlockAll(boxes))
